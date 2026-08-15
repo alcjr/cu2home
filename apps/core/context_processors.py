ebf22a3452650678cec8a544b9677836721c5788
alcjr <cyberaxiom@gmail.com>
2026-08-15 20:32:38 +0200

    20:32:38 / 15-08-2026

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -5,8 +5,8 @@ def site_settings(request):
     Context processor para añadir variables globales a todos los templates.
     """
     return {
-        'SITE_NAME': getattr(settings, 'SITE_NAME', 'cu2home'),
-        'SITE_DESCRIPTION': getattr(settings, 'SITE_DESCRIPTION', 'Portal inmobiliario en Cuba'),
+        'site_name': getattr(settings, 'SITE_NAME', 'cu2home'),
+        'site_description': getattr(settings, 'SITE_DESCRIPTION', 'Portal inmobiliario en Cuba'),
         'LANGUAGES': settings.LANGUAGES,
         'LANGUAGE_CODE': getattr(request, 'LANGUAGE_CODE', settings.LANGUAGE_CODE),
     }
--- apps/properties/context_processors.py
+++ apps/properties/context_processors.py
@@ -1,12 +1,7 @@
-from django.conf import settings
+from .models import Property
 
-def site_settings(request):
-    """
-    Context processor para añadir variables globales a todos los templates.
-    """
+def featured_properties(request):
+    """Context processor para disponer de propiedades destacadas en cualquier template."""
     return {
-        'site_name': getattr(settings, 'SITE_NAME', 'cu2home'),
-        'site_description': getattr(settings, 'SITE_DESCRIPTION', 'Portal inmobiliario en Cuba'),
-        'LANGUAGES': settings.LANGUAGES,
-        'LANGUAGE_CODE': getattr(request, 'LANGUAGE_CODE', settings.LANGUAGE_CODE),
+        'featured_properties': Property.get_featured(limit=6),
     }
